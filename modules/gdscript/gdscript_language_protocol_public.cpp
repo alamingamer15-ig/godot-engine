bc60057ade0b802c9fe55674c2f9efe7c7711e8d
alamingamer15-ig <alamingamer15@gmail.com>
2026-09-25 23:53:44 +0600

    Fix copyright notice formatting
    Corrected copyright notice formatting in the file.

--- modules/gdscript/gdscript_language_protocol_public.cpp
+++ modules/gdscript/gdscript_language_protocol_public.cpp
@@ -5,7 +5,7 @@
 /*                             GODOT ENGINE                               */
 /*                        https://godotengine.org                         */
 /**************************************************************************/
-/* Copyright (c) 2026 Alamin, All contributors and creators
+/* Copyright (c) 2026 Alamin, All contributors and creators.              */
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
@@ -28,7 +28,6 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
-
 #include "gdscript_language_protocol_public.h"
 
 #ifdef TOOLS_ENABLED
