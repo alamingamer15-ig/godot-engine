db5460b73eb342811746beed5d64878c14aed054
alamingamer15-ig <alamingamer15@gmail.com>
2026-09-26 08:58:23 +0600

    Rename public GDScript API class

--- modules/gdscript/gdscript_language_protocol_public.cpp
+++ modules/gdscript/gdscript_language_protocol_public.cpp
@@ -36,21 +36,21 @@
 #include "core/io/file_access.h"
 #include "core/io/json.h"
 
-void GDScriptLanguageProtocolPublic::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("get_extension_apis"), &GDScriptLanguageProtocolPublic::get_extension_apis);
-	ClassDB::bind_method(D_METHOD("read_extension_apis", "path"), &GDScriptLanguageProtocolPublic::read_extension_apis);
-	ClassDB::bind_method(D_METHOD("save_extension_apis", "path"), &GDScriptLanguageProtocolPublic::save_extension_apis);
+void GDScriptLanguageAPI::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_extension_apis"), &GDScriptLanguageAPI::get_extension_apis);
+	ClassDB::bind_method(D_METHOD("read_extension_apis", "path"), &GDScriptLanguageAPI::read_extension_apis);
+	ClassDB::bind_method(D_METHOD("save_extension_apis", "path"), &GDScriptLanguageAPI::save_extension_apis);
 }
 
-Dictionary GDScriptLanguageProtocolPublic::get_extension_apis() {
+Dictionary GDScriptLanguageAPI::get_extension_apis() {
 	return GDExtensionAPIDump::generate_extension_api();
 }
 
-Dictionary GDScriptLanguageProtocolPublic::read_extension_apis(const String &p_path) {
+Dictionary GDScriptLanguageAPI::read_extension_apis(const String &p_path) {
 	Error error = OK;
 	String text = FileAccess::get_file_as_string(p_path, &error);
 	if (error != OK) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: Could not open API dump '%s'.", p_path));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: Could not open API dump '%s'.", p_path));
 		return Dictionary();
 	}
 
@@ -58,20 +58,20 @@ Dictionary GDScriptLanguageProtocolPublic::read_extension_apis(const String &p_p
 	json.instantiate();
 	error = json->parse(text);
 	if (error != OK) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: Error parsing '%s' at line %d: %s", p_path, json->get_error_line(), json->get_error_message()));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: Error parsing '%s' at line %d: %s", p_path, json->get_error_line(), json->get_error_message()));
 		return Dictionary();
 	}
 
 	Variant data = json->get_data();
 	if (data.get_type() != Variant::DICTIONARY) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: API dump '%s' does not contain a Dictionary.", p_path));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: API dump '%s' does not contain a Dictionary.", p_path));
 		return Dictionary();
 	}
 
 	return data;
 }
 
-Error GDScriptLanguageProtocolPublic::save_extension_apis(const String &p_path) {
+Error GDScriptLanguageAPI::save_extension_apis(const String &p_path) {
 	Ref<FileAccess> file = FileAccess::open(p_path, FileAccess::WRITE);
 	if (file.is_null()) {
 		return FileAccess::get_open_error();
@@ -83,7 +83,7 @@ Error GDScriptLanguageProtocolPublic::save_extension_apis(const String &p_path) 
 	Error error = OK;
 	String text = FileAccess::get_file_as_string(p_path, &error);
 	if (error != OK) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: Could not read generated API dump '%s'.", p_path));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: Could not read generated API dump '%s'.", p_path));
 		return error;
 	}
 
@@ -91,12 +91,12 @@ Error GDScriptLanguageProtocolPublic::save_extension_apis(const String &p_path) 
 	json.instantiate();
 	error = json->parse(text);
 	if (error != OK) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: Generated API dump '%s' is invalid JSON at line %d: %s", p_path, json->get_error_line(), json->get_error_message()));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: Generated API dump '%s' is invalid JSON at line %d: %s", p_path, json->get_error_line(), json->get_error_message()));
 		return error;
 	}
 
 	if (json->get_data().get_type() != Variant::DICTIONARY) {
-		ERR_PRINT(vformat("GDScriptLanguageProtocolPublic: Generated API dump '%s' is not a Dictionary.", p_path));
+		ERR_PRINT(vformat("GDScriptLanguageAPI: Generated API dump '%s' is not a Dictionary.", p_path));
 		return ERR_INVALID_DATA;
 	}
 
